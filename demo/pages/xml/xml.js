c0ad6c11525e14c76843eb888be18b9c6c43a2a9
TMD <kk412027247@gmail.com>
2019-03-26 22:58:24 +0800

    逻辑与法 条件逻辑 列表渲染 模板 引用 共同属性

--- demo/pages/xml/xml.js
+++ demo/pages/xml/xml.js
@@ -2,6 +2,41 @@ Page({
   data:{
     condition:true,
     length:6,
-    array:[{message:'foo'},{message:'bar'}]
+    array:[{message:'foo'},{message:'bar'}],
+    objectArray:[
+      { id: 5, quique: 'unique_5' },
+      { id: 4, quique: 'unique_4' },
+      { id: 3, quique: 'unique_3' },
+      { id: 2, quique: 'unique_2' },
+      { id: 1, quique: 'unique_1' },
+      { id: 0, quique: 'unique_0' },
+    ],
+    numberArray:[1,2,3,4]
+  },
+  swtich:function(e){
+    const length = this.data.objectArray.length
+    for(let i = 0; i< length; i++){
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random()*length)
+      const temp = this.data.objectArray[x]
+      this.data.objectArray[x] = this.data.objectArray[y]
+      this.data.objectArray[y] = temp
+    }
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addToFront: function(e){
+    const length = this.data.objectArray.length
+    this.data.objectArray = [{id: length, unique:'unique_'+length}].concat(this.data.objectArray)
+    this.setData({
+      objectArray:this.data.objectArray
+    })
+  },
+  addNumberToFront:function(e){
+    this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
+    this.setData({
+      numberArray: this.data.numberArray
+    })
   }
 })
